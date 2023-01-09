d21ff716db34e1cdbe0ecb2ebe6fd6dd348abdc2
Iriedson Souto <iriedson.souto@academico.ifpb.edu.br>
2023-01-09 19:25:56 -0300

    :tada: Initial commit

---
+++ src/main/java/br/com/rpe/iriedson/desafioestagiario2/DesafioEstagiario2Application.java
@@ -0,0 +1,13 @@
+package br.com.rpe.iriedson.desafioestagiario2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEstagiario2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEstagiario2Application.class, args);
+	}
+
+}
